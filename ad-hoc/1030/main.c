60ee1ed2e673161bc083bca48cb11f9bcee10d12
JoaoVSouto <joaovitorss99@hotmail.com>
2019-07-29 19:16:59 -0300

    Adds 1030

--- ad-hoc/1030/main.c
+++ ad-hoc/1030/main.c
@@ -1,46 +1,31 @@
 #include <stdio.h>
+int josephusLoop(int n, int k, int startingPoint) {
+  if (n == 1)
+    return 1;
 
-int giveNumberOnCircle(int fromNumber, int step, int circleSize) {
-  int sum = fromNumber + step;
-  if (sum > circleSize) {
-    return sum - circleSize;
+  int newSp, survivor;
+  newSp = (startingPoint + k - 2) % n + 1;
+  survivor = josephusLoop(n - 1, k, newSp);
+
+  if (survivor < newSp) {
+    return survivor;
+  } else {
+    return survivor + 1;
   }
-  return sum;
+}
+
+int josephus(int n, int k) {
+  return josephusLoop(n, k, 1);
 }
 
 int main() {
-  int numberOfPeople, step, i;
+  int nc, n, k, s, i;
+  scanf("%d", &nc);
 
-  scanf("%d %d", &numberOfPeople, &step);
-
-  int people[numberOfPeople], remainingPeople = numberOfPeople, kill = step, track = step, count = kill;
-
-  // fill the array with crescent order of numbers
-  // i.e 5 => [1, 2, 3, 4, 5]
-  for (i = 1; i <= numberOfPeople; i++)
-    people[i - 1] = i;
-
-  while (remainingPeople != 1) {
-    remainingPeople--;
-    people[kill - 1] = 0;
-    count = kill;
-    do {
-      track = giveNumberOnCircle(count, 1, numberOfPeople);
-      count++;
-    } while (people[track - 1] == 0);
-    count = track;
-    do {
-      kill = giveNumberOnCircle(count, step - 1, numberOfPeople);
-      count++;
-    } while (people[kill - 1] == 0);
-  }
-  // [1, 2, 3, 4, 5, 6] s: 3 => 1
-  // [1, 2, 3, 4, 5, 6]
-
-  for (i = 0; i < numberOfPeople; i++) {
-    if (people[i] != 0) {
-      printf("caba que n morreu: %d\n", people[i]);
-    }
+  for (i = 1; i <= nc; i++) {
+    scanf("%d %d", &n, &k);
+    s = josephus(n, k);
+    printf("Case %d: %d\n", i, s);
   }
 
   return 0;
