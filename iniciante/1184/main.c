8054dc016f1ee7ac925dd40c993b781a1178ce3d
JoaoVSouto <joaovitorss99@hotmail.com>
2019-08-26 08:48:18 -0300

    Adds 1184

---
+++ iniciante/1184/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define SIZE 12
+
+float sumMatrix(float matrix[SIZE][SIZE], short *counter) {
+  float sum = 0;
+  short i, j;
+
+  for (i = 0; i < SIZE; i++) {
+    for (j = 0; j < SIZE; j++) {
+      if (i <= j) continue;
+      sum += matrix[i][j];
+      *counter += 1;
+    }
+  }
+
+  return sum;
+}
+
+int main() {
+  float N[SIZE][SIZE], result;
+  char O;
+  short i, j, counter = 0;
+
+  scanf("%c", &O);
+
+  for (i = 0; i < SIZE; i++) {
+    for (j = 0; j < SIZE; j++) {
+      scanf("%f", &N[i][j]);
+    }
+  }
+
+  result = sumMatrix(N, &counter);
+
+  if (O == 'M') {
+    result /= counter;
+  }
+
+  printf("%.1f\n", result);
+
+  return 0;
+}
