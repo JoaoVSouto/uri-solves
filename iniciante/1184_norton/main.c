8f63a6c680b139192fe6674b20c9c27c6c6d5617
Lincoln500 <norton.maracaja@gmail.com>
2019-08-26 15:44:28 -0300

    Adiciona 1184

---
+++ iniciante/1184_norton/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define POS 12
+
+int main() {
+    float soma = 0.0, M[POS][POS];
+    int i, j, contador = 0;
+    char operacao;
+    scanf(" %c", &operacao);
+    for (i = 0; i < POS; i++) {
+        for (j = 0; j < POS; j++) {
+            scanf("%f", &M[i][j]);
+            if (i > j) {
+                soma += M[i][j];
+                contador++;
+            }
+        }
+    }
+    switch (operacao) {
+        case 'S':
+            printf("%.1f\n", soma);
+            break;
+        case 'M':
+            printf("%.1f\n", (soma / contador));
+            break;
+    }
+
+    return 0;
+}
