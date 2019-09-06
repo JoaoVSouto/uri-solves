9389098f7499f593cb433a227bcc6ea8e615cc7b
JOAO VITOR SOUTO DOS SANTOS <jvitor990@A101-13L.imd.local>
2019-09-06 12:23:45 -0300

    Adds malloc

---
+++ sala/malloc/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int tam, i;
+  int *vetor;
+
+  printf("Digite o tamanho do array: ");
+  scanf("%d", &tam);
+
+  vetor = (int *)malloc(sizeof(int) * tam);
+
+  for (i = 0; i < tam; i++) {
+    scanf("%d", &vetor[i]);
+  }
+
+  for (i = 0; i < tam; i++) {
+    printf("%d ", vetor[i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
