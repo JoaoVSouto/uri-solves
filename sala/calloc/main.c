9bf924e72307c6779bc60f749d647306e45881af
JOAO VITOR SOUTO DOS SANTOS <jvitor990@A101-10L.imd.local>
2019-09-09 11:13:11 -0300

    Adds calloc

---
+++ sala/calloc/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int tam, i;
+  int *vetor;
+
+  printf("Digite o tamanho do array: ");
+  scanf("%d", &tam);
+
+  vetor = (int *)calloc(tam, sizeof(int));
+
+  for (i = 0; i < tam; i++) {
+    scanf("%d", &vetor[i]);
+  }
+
+  for (i = 0; i < tam; i++) {
+    printf("%d ", vetor[i]);
+  }
+
+  printf("\n");
+
+  free(vetor);
+
+  return 0;
+}
