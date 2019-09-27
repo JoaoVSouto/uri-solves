8eb3d3ebca4fd3da1b13e175d34de9acfea2b7c4
JOAO VITOR SOUTO DOS SANTOS <jvitor990@A101-10L.imd.local>
2019-09-27 11:51:40 -0300

    :construction: Starts palindromo_recursivo

---
+++ sala/palindromo_recursivo/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+void palindrome(int strLen, int counter, char prevChar, char* string, char* stringRev) {
+  if (counter == strLen - 1) {
+    stringRev[strLen - 1 - counter] = string[counter];
+    return;
+  }
+
+  palindrome(strLen, counter + 1, string[counter + 1], string, stringRev);
+
+  stringRev[strLen - 1 - counter] = string[counter];
+}
+
+int main() {
+  char string[30];
+  char stringRev[30];
+  int i;
+
+  printf("Digite uma palavra: ");
+  scanf("%s", string);
+
+  for (i = 0; string[i] != '\0'; i++)
+    ;
+
+  palindrome(i, 0, string[0], string, stringRev);
+
+  if (!strcmp(string, stringRev)) {
+    printf("São palíndromos!\n");
+  } else {
+    printf("Não são palíndromos!\n");
+  }
+
+  return 0;
+}
